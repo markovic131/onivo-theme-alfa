6fdaafb15f6af0934a2459baf03bc10e08da3f59
Marko Aleksic <psybaron@gmail.com>
2015-10-24 10:59:07 +0200

    implemented gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,29 @@
+'use strict';
+
 var gulp   = require('gulp');
 var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 // var sass   = require('gulp-sass');
 // var watch  = require('gulp-watch');
 
-// gulp.task('compress', function() {
-//   gulp.src('lib/*.js')
-//     .pipe(uglify())
-//     .pipe(gulp.dest('dist'))
-// });
-
-
-// gulp.task('sass', function() {
-//     return gulp.src('assets/sass/*.scss')
-//         .pipe(sass())
-//         .pipe(gulp.dest('public/css'));
+// gulp.task('sass', function () {
+//   return gulp.src('assets/scss/*.scss')
+//     .pipe(sass())
+//     .pipe(gulp.dest('css'));
 // });
 
 gulp.task('compress', function() {
-    return gulp.src('assets/js/app.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('public/js'));
+    return gulp.src([
+        'assets/js/bootstrap.min.js',
+        'assets/js/app.js',
+    ])
+    .pipe(concat('app.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('js'));
 });
 
-// gulp.task('js-onivo',function(){
-//     return gulp.src([
-//             'assets/js/support.js',
-//             'assets/js/app.js',
-//             'assets/js/directives.js',
-//             'assets/js/filters.js',
-//             'assets/js/controllers.js'
-//         ])
-//         .pipe(concat('onivo.min.js'))
-//         .pipe(uglify())//{sourceMap:true}
-//         .pipe(gulp.dest(jsBuildDir));
-// });
-
 // gulp.task('watch', function () {
-//     gulp.watch('assets/sass/**/*.scss', ['sass']);
+//     //gulp.watch('assets/scss/**/*.scss', ['sass']);
 //     gulp.watch('assets/js/**/*.js', ['compress']);
 // });
 
